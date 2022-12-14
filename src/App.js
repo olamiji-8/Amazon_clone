772f674f5bd68ef1857db3570d77eb139b8758ca
olamiji-8 <olamijisotomi@gmail.com>
2022-12-14 23:32:07 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import {BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-     
+     <Routes>
+      <Route path={}>
+     </Routes>
     </div>
+    </Router>
   );
 }
 
