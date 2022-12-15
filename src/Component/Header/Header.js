251183b0091f00ca60a6be0ddb5d22420997c481
olamiji-8 <olamijisotomi@gmail.com>
2022-12-15 21:11:46 +0100

    update

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -5,7 +5,7 @@ import Amazon from "../../Asset/amazon.png"
 const Header = () => {
   return (
     <nav className='header'>
-      <img src={Amazon} alt='logo' className='header__logo' width="140px" height="70px" style={{marginTop:"0px"}}/>
+      <img src={Amazon} alt='logo' className='header__logo' width="140px" height="70px" style={{marginTop:"0px", cursor:"pointer"}}/>
       
     </nav>
   )
