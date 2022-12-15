e257f0f54fc74c2a020904cd455734a884e73b16
olamiji-8 <olamijisotomi@gmail.com>
2022-12-15 21:20:09 +0100

    logo

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./Header.css"
 import Amazon from "../../Asset/amazon.png"
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <nav className='header'>
+      <Link to= "/"> 
       <img src={Amazon} alt='logo' className='header__logo' width="140px" height="70px" style={{marginTop:"0px", cursor:"pointer"}}/>
-      
+      </Link>
     </nav>
   )
 }
