1d637ad5ead7ae7c7ba8b7699c06822c57066645
olamiji-8 <olamijisotomi@gmail.com>
2023-01-06 16:09:37 +0100

    update

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import AdvertisementOne from '../../Component/AdvertisementOne/AdvertisementOne'
+import AdvertisementFour from '../../Component/AdvertisementFour/AdvertisementFour'
 import "./Home.css"
 
 
@@ -8,10 +9,14 @@ const Home = () => {
     <div className='Home'>
         <div style={{paddingTop:"260px", display:"flex",flexWrap:"wrap"}}>
           <AdvertisementOne/>
+          <AdvertisementFour/>
           <AdvertisementOne/>
           <AdvertisementOne/>
+          {/* <AdvertisementFour/> */}
           <AdvertisementOne/>
+          {/* <AdvertisementFour/> */}
           <AdvertisementOne/>
+          {/* <AdvertisementFour/> */}
         </div>
     </div>
   )
