90cc760fb5446bbc78e5474edee91ae6c362ab69
olamiji-8 <olamijisotomi@gmail.com>
2023-01-13 19:46:25 +0100

    update

--- src/Pages/Signin/Signin.js
+++ src/Pages/Signin/Signin.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import "./Signin.css"
+import Logo from "../../Asset/amazonn.png"
 
 const Signin = () => {
   return (
-    <div>Signin</div>
+    <div>
+      <img src={Logo} alt='#'  height='30px' className='Signin__logo'/>
+    </div>
   )
 }
 
