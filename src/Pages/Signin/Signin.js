bc925f8c24cd49c191e7f39980bc3ad5d5c22550
olamiji-8 <olamijisotomi@gmail.com>
2023-01-13 20:13:49 +0100

    update

--- src/Pages/Signin/Signin.js
+++ src/Pages/Signin/Signin.js
@@ -6,6 +6,19 @@ const Signin = () => {
   return (
     <div>
       <img src={Logo} alt='#'  height='30px' className='Signin__logo'/>
+      <div>
+        <p>Sign in</p>
+
+        <form>
+          <label>Email or mobile phone number <input type="text"/></label>
+          <button>Continue</button>
+        </form>
+        <p>By continuing, you agree to Amazon's Conditions of Use and Privacy Notice.</p>
+      </div>
+      <div>
+        <p>New to Amazon?</p>
+        <button>Create your Amazon account</button>
+      </div>
     </div>
   )
 }
--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import "./Signup.css"
+import Logo from "../../Asset/amazonn.png"
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div>
+         <img src={Logo} alt='#'  height='30px' className='Signup__logo'/>
+         <div>
+            <p>Create account</p>
+            <label> Your name <input type="text"  placeholder='First and last name'/></label>
+            <label>Mobile number or email <input type="text" /></label>
+            <label>Password <input type="password" placeholder='At least 6 characters' minLength={6}/></label>
+            <p>Passwords must be at least 6 characters.</p>
+            <label>Re-enter password <input type="password"  minLength={6}/></label>
+            <button>Continue</button>
+         </div>
+         <div>
+            <p>By creating an account, you agree to Amazon's Conditions of Use and Privacy Notice.</p>
+         </div>
+         <div>
+            <p>Already have an account? Sign in</p>
+         </div>
+    </div>
   )
 }
 
