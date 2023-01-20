4b630f042d273a49ef355d7c94f1e3efe2954cee
olamiji-8 <olamijisotomi@gmail.com>
2023-01-20 16:18:48 +0100

    update

--- src/Component/GiftCard/GiftCardRight.js
+++ src/Component/GiftCard/GiftCardRight.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from 'react';
+import Data from "../../Data";
 
 const GiftCardRight = () => {
+
   return (
     <div>
+      <p>Shop the perfect gift card</p>
+      <div>
+        <p>Shop by occasion</p>
+        <div>
+          <button>New year's</button>
+          <button>Valentine's day</button>
+          <button>Birthday</button>
+          <button>Thank you</button>
+          <button>Work place</button>
+          <button>New baby</button>
+        </div>
+      </div>
+      <div>
+        <p>Shop by card type</p>
+        </div>
+      {Data.map(Data =>{
+        return(
+          <div>
+            <img src={Data.img} alt=""/>
+            <h2>{Data.title}</h2>
+            <h5>{Data.description}</h5>
+
+          </div>
+        )
+      })}
       
     </div>
   )
--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-export const Data =[
+const Data = [
     {
         id:1,
         img:"https://firebasestorage.googleapis.com/v0/b/clone-b643d.appspot.com/o/Amazon%20gift%20card%2Fe1.jpg?alt=media&token=9cc56e8d-43b1-4746-8829-61463bada326",
@@ -35,6 +35,8 @@ export const Data =[
 
     }
 ]
+export default Data;
+
 export const Info=[
     {
         id:1,
