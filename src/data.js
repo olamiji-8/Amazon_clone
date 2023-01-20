ea405ce4b687857b91565397b16ef2afab677d89
olamiji-8 <olamijisotomi@gmail.com>
2023-01-20 11:22:36 +0100

    update

---
+++ src/data.js
@@ -0,0 +1,63 @@
+export const Data =[
+    {
+        id:1,
+        img:"https://firebasestorage.googleapis.com/v0/b/clone-b643d.appspot.com/o/Amazon%20gift%20card%2Fe1.jpg?alt=media&token=9cc56e8d-43b1-4746-8829-61463bada326",
+        title: "eGift Cards",
+        description:"Add your personal message and send by email or text."
+
+    },
+    {
+        id:2,
+        img:"https://firebasestorage.googleapis.com/v0/b/clone-b643d.appspot.com/o/Amazon%20gift%20card%2Fe2.jpg?alt=media&token=8794d372-2f82-4302-9b64-75570fcfffa9",
+        title: "Physical gift cards",
+        description:"Send a smile with unique designs and fun packaging."
+
+    },
+    {
+        id:3,
+        img:"https://firebasestorage.googleapis.com/v0/b/clone-b643d.appspot.com/o/Amazon%20gift%20card%2Fe3.jpg?alt=media&token=9d65101f-cf4c-4608-b1b4-9cc9b89fc70b",
+        title: "Print at home",
+        description:"Print and fold an Amazon Gift Card at home."
+
+    },
+    {
+        id:4,
+        img:"https://firebasestorage.googleapis.com/v0/b/clone-b643d.appspot.com/o/Amazon%20gift%20card%2Fe4.jpg?alt=media&token=958c3030-4ae8-414a-80a9-46450aae547d",
+        title: "Specialty gift cards",
+        description:"Give a gift from their favorite brand."
+
+    },
+    {
+        id:5,
+        img:"https://firebasestorage.googleapis.com/v0/b/clone-b643d.appspot.com/o/Amazon%20gift%20card%2Fe7.jpg?alt=media&token=a98a3fe8-b0cd-494d-8b93-d25628571537",
+        title: "Amazon Reload",
+        description:"Easily add funds to your gift card balance."
+
+    }
+]
+export const Info=[
+    {
+        id:1,
+        img:"https://firebasestorage.googleapis.com/v0/b/clone-b643d.appspot.com/o/Amazon%20gift%20card%2Fd1.jpg?alt=media&token=b0d3458c-9e08-4b64-836a-595c45805c94",
+        title: "Amazon",
+        description:"Easily add funds to your gift card balance.",
+    },
+    {
+        id:2,
+        img:"https://firebasestorage.googleapis.com/v0/b/clone-b643d.appspot.com/o/Amazon%20gift%20card%2Fd1.jpg?alt=media&token=b0d3458c-9e08-4b64-836a-595c45805c94",
+        title: "Amazon",
+        description:"Easily add funds to your gift card balance."
+    },
+    {
+        id:3,
+        img:"https://firebasestorage.googleapis.com/v0/b/clone-b643d.appspot.com/o/Amazon%20gift%20card%2Fd2.jpg?alt=media&token=d96d0100-873c-47fe-83c6-16513c4db17e",
+        title: "Amazon",
+        description:"Easily add funds to your gift card balance."
+    },
+    {
+        id:4,
+        img:"https://firebasestorage.googleapis.com/v0/b/clone-b643d.appspot.com/o/Amazon%20gift%20card%2Fd4.jpg?alt=media&token=d9ad5c24-14aa-42bc-97f6-5db7c48b46fa",
+        title: "Amazon",
+        description:"Easily add funds to your gift card balance."
+    }
+]
