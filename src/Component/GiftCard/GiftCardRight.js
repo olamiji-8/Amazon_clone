1d2bf15d6131c8f62cff4e4bb1cad19241348962
olamiji-8 <olamijisotomi@gmail.com>
2023-02-02 19:59:08 +0100

    update

--- src/Component/GiftCard/GiftCardRight.js
+++ src/Component/GiftCard/GiftCardRight.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Data from "../../Data";
+import  "./GiftCardRight.css";
 
 const GiftCardRight = () => {
 
@@ -22,12 +23,13 @@ const GiftCardRight = () => {
         </div>
       {Data.map(Data =>{
         return(
-          <div>
-            <img src={Data.img} alt=""/>
+          <div className='GiftCardRight_Item'>
+            <div className='GiftCardRight_ItemFlex'>
+            <img src={Data.img} alt="" className='GiftCardRight_ItemImage'width="30%" height="80%"/>
             <h2>{Data.title}</h2>
             <h5>{Data.description}</h5>
-
-          </div>
+            </div>
+            </div>
         )
       })}
       
