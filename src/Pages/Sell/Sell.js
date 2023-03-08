8679d51392e733f885352b147806c7ff75dd9a03
olamiji-8 <olamijisotomi@gmail.com>
2023-03-08 14:33:03 +0100

    update

---
+++ src/Pages/Sell/Sell.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sell = () => {
+  return (
+    <div>
+        <div>
+            <p>Sell on Amazon</p>
+        </div>
+    </div>
+  )
+}
+
+export default Sell
