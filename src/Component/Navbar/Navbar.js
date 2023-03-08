37515f2487fa0b1a045c1a193afe168aeca5ce55
olamiji-8 <olamijisotomi@gmail.com>
2023-03-08 14:00:33 +0100

    update

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -16,19 +16,19 @@ const Navbar = () => {
         </Link>
       </div>
       <div className="navbar__text">
-        <Link>
+        <Link to="/Deals">
           <p className="navbar__text__hover">Today's Deals</p>
         </Link>
-        <Link>
+        <Link to="/Customer Service">
           <p className="navbar__text__hover">Customer Service</p>
         </Link>
-        <Link>
+        <Link to="/Gift Card">
           <p className="navbar__text__hover">Gift Cards</p>
         </Link>
         <Link to="/Registry">
           <p className="navbar__text__hover">Registry</p>
         </Link>
-        <Link>
+        <Link to="/Sell">
           <p className="navbar__text__hover">Sell</p>
         </Link>
       </div>
