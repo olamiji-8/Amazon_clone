0f6f5927824e795e811fad2649190874a14e51f9
olamiji-8 <olamijisotomi@gmail.com>
2023-03-08 13:58:12 +0100

    update

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,29 +1,39 @@
-import React from 'react'
-import "./Navbar.css"
-import MenuIcon from '@mui/icons-material/Menu';
-import { Link } from 'react-router-dom'
-import Product from '../../Pages/Product.js/Product';
+import React from "react";
+import "./Navbar.css";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Link } from "react-router-dom";
+import Product from "../../Pages/Product.js/Product";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-      <div className='navbar__icon'>
-        <span><MenuIcon/></span>
-        <Link to= "/Product">
-        <span>All</span>
+    <div className="navbar">
+      <div className="navbar__icon">
+        <span>
+          <MenuIcon />
+        </span>
+        <Link to="/Product">
+          <span>All</span>
         </Link>
-        </div>
-        <div className='navbar__text'>
-        <p className='navbar__text__hover'>Today's Deals</p>
-        <p className='navbar__text__hover'>Customer Service</p>
-        <p className='navbar__text__hover'>Gift Cards</p>
-        <Link to= "/Registry">
-        <p className='navbar__text__hover'>Registry</p>
-        </Link>
-        <p className='navbar__text__hover'>Sell</p>
-        </div>
       </div>
-  )
-}
+      <div className="navbar__text">
+        <Link>
+          <p className="navbar__text__hover">Today's Deals</p>
+        </Link>
+        <Link>
+          <p className="navbar__text__hover">Customer Service</p>
+        </Link>
+        <Link>
+          <p className="navbar__text__hover">Gift Cards</p>
+        </Link>
+        <Link to="/Registry">
+          <p className="navbar__text__hover">Registry</p>
+        </Link>
+        <Link>
+          <p className="navbar__text__hover">Sell</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
